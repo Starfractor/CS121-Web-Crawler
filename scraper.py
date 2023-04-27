81d35e304f92bcc074f35f11ae66ed76fe9e968b
CyrsLiu <dajcbcyrus@gmail.com>
2023-04-26 18:34:52 -0700

    test

--- scraper.py
+++ scraper.py
@@ -1,15 +1,30 @@
 import re
 from urllib.parse import urlparse, urljoin, urldefrag
+from collections import defaultdict
 from bs4 import BeautifulSoup
-
+from urllib.robotparser import RobotFileParser
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+    if links:
+        return [link for link in links if is_valid(link)]
+    else:
+        return []
 
+robot_parsers = {}
+
+def get_robot_parser(url):
+    domain = urlparse(url).scheme + '://' + urlparse(url).hostname
+    if domain not in robot_parsers:
+        robot_parser = RobotFileParser()
+        robot_parser.set_url(urljoin(domain, '/robots.txt'))
+        robot_parser.read()
+        robot_parsers[domain] = robot_parser
+
+    return robot_parsers[domain]
 
 def extract_next_links(url, resp):
-    # Implementation required.
+     # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
     # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
@@ -18,20 +33,24 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+
+    # Detect and avoid dead URLs that return a 200 status but no data
     if resp.status != 200 or resp.error:
         return []
 
     soup = BeautifulSoup(resp.raw_response.content, "html.parser")
     links = [link.get("href") for link in soup.find_all("a")]
 
-    # Transform relative URLs to absolute URLs
-    absolute_links = [urljoin(resp.url, link) for link in links]
+    allowed_links = []
+    for link in links:
+        absolute_url = urljoin(url, link)
+        defrag_url, _ = urldefrag(absolute_url)
+        robot_parser = get_robot_parser(defrag_url)
+        if robot_parser.can_fetch("*", defrag_url):
+            allowed_links.append(defrag_url)
 
-    # Defragment the URLs
-    defragmented_links = [urldefrag(link).url for link in absolute_links]
-
-    return defragmented_links
-
+    return allowed_links
+ 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
@@ -40,11 +59,10 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-
+        
         # Crawl only the specified domains and paths stated in the assignment
-        if not re.match(r"^(?:http|https)://(?:www\.)?(?:ics\.uci\.edu|cs\.uci\.edu|informatics\.uci\.edu|stat\.uci\.edu)/.*", url):
+        if not re.match(r".*\.ics\.uci\.edu/.*|.*\.cs\.uci\.edu/.*|.*\.informatics\.uci\.edu/.*|.*\.stat\.uci\.edu/.*", url):
             return False
-
         
         if urlIsInvalid(url):
             return False
@@ -60,9 +78,11 @@ def is_valid(url):
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
 
     except TypeError:
-        print("TypeError for ", parsed)
+        print ("TypeError for ", parsed)
         raise
 
+
+# These functions are for trap prevention
 
 def urlIsInvalid(url):
     # Additional bad queries
